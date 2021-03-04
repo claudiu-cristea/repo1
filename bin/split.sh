586f2d85b492ffda396646e88177c27d351ed433
Claudiu Cristea <clau.cristea@gmail.com>
2021-03-04 20:13:38 +0200

    Add bins.

---
+++ bin/split.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+CURRENT_BRANCH="master"
+
+function split()
+{
+    SHA1=`./bin/splitsh-lite --prefix=$1`
+    git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
+}
+
+function remote()
+{
+    git remote add $1 $2 || true
+}
+
+git pull origin $CURRENT_BRANCH
+
+remote repo1 git@github.com:claudiu-cristea/repo1.git
+
+split 'packages/repo1' repo1
